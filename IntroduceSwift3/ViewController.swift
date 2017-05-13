1e8dff38f809b21f164c0de3b78a04c3bd434277
Moises <moisesapaza07@gmail.com>
2017-05-13 03:40:51 -0300

    first commit

---
+++ IntroduceSwift3/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  IntroduceSwift3
+//
+//  Created by Moises on 4/19/17.
+//  Copyright © 2017 Moises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numA: UITextField!
+    @IBOutlet weak var numB: UITextField!
+    @IBOutlet weak var result: UITextField!
+    
+    @IBOutlet weak var operatorText: UILabel!
+    @IBOutlet weak var operatorSegment: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func operateFunction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func operateWIthSegmentController(_ sender: Any) {
+        if(verifyNumbers()){
+            switch operatorSegment.selectedSegmentIndex {
+            case 0:
+                let res = getNumbers().numA + getNumbers().numB
+                result.text = String(res)
+                operatorText.text = "+"
+            case 1:
+                let res = getNumbers().numA - getNumbers().numB
+                result.text = String(res)
+                operatorText.text = "-"
+            case 2:
+                let res = getNumbers().numA * getNumbers().numB
+                result.text = String(res)
+                operatorText.text = "*"
+            default:
+                let res = getNumbers().numA / getNumbers().numB
+                result.text = String(res)
+                operatorText.text = "/"
+            }
+        }else{
+            createAlert(title: "Error", message: "Missing number")
+        }
+        
+    }
+    
+    func getNumbers() -> (numA: Double, numB: Double) {
+        return (Double(numA.text!)!, Double(numB.text!)!)
+    }
+    
+    func verifyNumbers() -> Bool {
+        return (numA.text != "" && numB.text != "") == true
+    }
+    
+    func createAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
